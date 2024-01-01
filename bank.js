ea0eabc551a48dacbd9219fe2a873bfb28ac7b00
Samius Sazin <samiussazin45@gmail.com>
2024-01-02 01:32:29 +0600

    commit

--- bank.js
+++ bank.js
@@ -1,10 +1,11 @@
+//Checking password & userName
 document.getElementById('submit_button').addEventListener('click',  function(){
     const get_user_name = document.getElementById('user_name').value;
     const get_password = document.getElementById('pass_word').value;
     
     if(get_user_name == 'sazin' && get_password == 'sazin1234'){
         // alert('Tuu login ho geya re, tension nehi leneka ree');
-        
+
         document.getElementById('user_name').value = '';
         document.getElementById('pass_word').value = '';
 
