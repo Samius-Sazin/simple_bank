52ad26de7a4d92632c8ce07494541813c2204a7b
Samius Sazin <samiussazin45@gmail.com>
2024-01-02 11:33:20 +0600

    completed

--- js/banking.js
+++ js/banking.js
@@ -1,33 +1,39 @@
-// handle deposit button event
-document.getElementById('deposit_button').addEventListener('click', function(){
-    // get input value
-    const get_deposit_value = document.getElementById('deposit_input_area').value;
-
-    // set input value to deposit area
-    const new_previous_deposit = document.getElementById('previous_deposit');
-    new_previous_deposit.innerText = '$' + (parseInt(new_previous_deposit.innerText.substring(1)) + parseInt(get_deposit_value));
-
-    //update balance area
+//method that help "withdraw, deposit" button
+function transaction_haldle(get_trans_element, new_previous_trans, trans_type){
+    //get previous balance element
     const new_balance = document.getElementById('previous_balance');
-    new_balance.innerText = '$' + (parseInt(new_balance.innerText.substring(1)) + parseInt(get_deposit_value));
 
-    //clear the withdraw input area
-    document.getElementById('deposit_input_area').value = '';
+    //check if ammount is valid or not
+    if(get_trans_element.value === '' || isNaN(get_trans_element.value) || parseInt(get_trans_element.value) <= 0){
+        alert('Invalid Entry');
+    }
+    else if(trans_type == 'withdraw' && parseInt(get_trans_element.value) > parseInt(new_balance.innerText.substring(1))){
+        alert('Not enough balance');
+    }
+    else{
+        //update deposit/withdraw part
+        new_previous_trans.innerText = '$' + (parseInt(new_previous_trans.innerText.substring(1)) + parseInt(get_trans_element.value));
+
+        //update balance area
+        if(trans_type == 'deposit'){
+            new_balance.innerText = '$' + (parseInt(new_balance.innerText.substring(1)) + parseInt(get_trans_element.value));
+        }
+        else if(trans_type == 'withdraw'){
+            new_balance.innerText = '$' + (parseInt(new_balance.innerText.substring(1)) - parseInt(get_trans_element.value));
+        }
+    }
+    //clear the deposit/withdraw input area
+    get_trans_element.value = '';
+}
+
+// handle "deposit" button event
+document.getElementById('deposit_button').addEventListener('click', function(){
+    // passing parameters to function(deposit_element, prev_deposit_element)
+    transaction_haldle(document.getElementById('deposit_input_area'), document.getElementById('previous_deposit'), 'deposit');
 })
 
-// handle withdraw button event
+// handle "withdraw" button event
 document.getElementById('withdraw_button').addEventListener('click', function(){
-    // get withdraw value
-    const get_withdraw_value = document.getElementById('withdraw_input_area').value;
-
-    // set withdraw value to withdraw area
-    const new_previous_withdraw = document.getElementById('previous_withdraw');
-    new_previous_withdraw.innerText = '$' + (parseInt(new_previous_withdraw.innerText.substring(1)) + parseInt(get_withdraw_value));
-
-    //update balance area
-    const new_balance = document.getElementById('previous_balance');
-    new_balance.innerText = '$' + (parseInt(new_balance.innerText.substring(1)) - parseInt(get_withdraw_value));
-
-    //clear the withdraw input area
-    document.getElementById('withdraw_input_area').value = '';
+    // passing parameters to function(withdraw_element, prev_withdraw_element)
+    transaction_haldle(document.getElementById('withdraw_input_area'), document.getElementById('previous_withdraw'), 'withdraw');
 })
